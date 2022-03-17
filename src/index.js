be99cc8db8cfd1d58a71b955aeeaf88e071b79f7
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2022-03-18 04:50:25 +0530

    boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScrollEffects = () => {
+    return (
+        <div>
+            <h1>ScrollEffects</h1>
+        </div>
+    );
+}
+
+
+export default ScrollEffects
